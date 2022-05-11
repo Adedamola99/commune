60a8d9cea470570b2a0a6ea71de9d385f6dacbe0
Sebastian Felling <sebastian@felling.de>
2022-05-11 09:54:08 +0200

    optimized menu toggling on mobile devices

--- commune.js
+++ commune.js
@@ -1,15 +1,28 @@
 var mobileBtn = document.getElementById("mobileBtn");
-var ul = document.querySelector("ul");
-var plusExit = true
 
-mobileBtn.addEventListener("click", function() {
-    if (plusExit) {
-        var change = mobileBtn.setAttribute("src", "./Assets/exit.svg");
-        ul.classList.add("menu");
-        plusExit = false;
-    } else {
-        var change = mobileBtn.setAttribute("src", "./Assets/Hamburger Menu.svg");
-        ul.classList.remove("menu");
-        plusExit = true;
-    }
-});
+// if there is only one, let's use an Id here
+// otherwise we might get the wrong element (in case there will be more <ul>s later)
+// hint: you might have to update some css, I have only looked at the menu toggle function
+var mainMenuUl = document.getElementById("mainMenu");
+
+// we won't need this. let's just use classes to manage state
+// that way we keep DOM problems inside the DOM
+// var plusExit = true
+
+function toggleMainMenu()
+{
+    // you can use `classList.toggle()` to toggle an element
+    // as a bonus, it will directly return the new state
+    // hint: you might have to update some css, I have only looked at the menu toggle function
+    const isMenuVisibleNow = mainMenuUl.classList.toggle("visible");
+
+    /* 
+        this is a UI issue and should be dealt with via CSS
+        task: change your code so that you can handle this with CSS 
+              based on the fact of whether the mainMenu is currently toggled (visible) or not
+        hint: you might have to pull the class state up some nodes (e.g. onto the <body> element, using maybe a 'main-menu-visible' name)
+    */
+    mobileBtn.setAttribute("src", isMenuVisibleNow ? "./Assets/exit.svg" : "./Assets/Hamburger Menu.svg");
+}
+
+mobileBtn.addEventListener("click", toggleMainMenu);
